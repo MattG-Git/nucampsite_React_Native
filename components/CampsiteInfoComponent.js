2c3ec0d8814e0bc2d89767906ee705a3a7640e30
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-12-05 10:17:57 -0800

    React-Native wk 3 assignment

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList,Modal, Button, StyleSheet,Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -35,24 +35,20 @@ function RenderComments({comments}) {
     return (
         <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
             <Card title='Comments'>
-                <FlatList
-                    data={comments}
-                    renderItem={renderCommentItem}
-                    keyExtractor={item => item.id.toString()}
-                />
-            </Card>
-        </Animatable.View>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+      </Animatable.View>
     );
 }
-
 function RenderCampsite(props) {
-
     const {campsite} = props;
-
     const view = React.createRef();
-
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
-
+    const recognizeComment= ({dx}) => (dx > -200) ? true : false;
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -79,11 +75,12 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
     });
-
     if (campsite) {
         return (
             <Animatable.View
@@ -93,34 +90,33 @@ function RenderCampsite(props) {
                 ref={view}
                 {...panResponder.panHandlers}>
                 <Card
-                    featuredTitle={campsite.name}
-                    image={{uri: baseUrl + campsite.image}}>
-                    <Text style={{margin: 10}}>
-                        {campsite.description}
-                    </Text>
-                    <View style={styles.cardRow} >
-                        <Icon
-                            name={props.favorite ? 'heart' : 'heart-o'}
-                            type='font-awesome'
-                            color='#f50'
-                            raised
-                            reverse
-                            onPress={() => props.favorite ? 
-                                console.log('Already set as a favorite') : props.markFavorite()}
-                        />
-                        <Icon
-                            name={'pencil'}
-                            type='font-awesome'
-                            color='#5637DD'
-                            raised
-                            reverse
-                            onPress={() => props.onShowModal()}
-                        />
-                    </View>
-                </Card>
+                featuredTitle={campsite.name}
+                image={{uri: baseUrl + campsite.image}}>
+                <Text style={{margin: 10}}>
+                    {campsite.description}
+                </Text>
+                <View style={styles.cardRow} >
+                    <Icon
+                        name={props.favorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        raised
+                        reverse
+                        onPress={() => props.favorite ? 
+                            console.log('Already set as a favorite') : props.markFavorite()}
+                    />
+                    <Icon
+                        name={'pencil'}
+                        type='font-awesome'
+                        color='#5637DD'
+                        raised
+                        reverse
+                        onPress={() => props.onShowModal()}
+                    />
+                </View>
+            </Card>
             </Animatable.View>
-        )
-    }; 
+    )}; 
 }
 class CampsiteInfo extends Component {
     constructor(props) {
